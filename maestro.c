7d8ebf378de169c177f0efd78131d8089731351e
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2025-02-10 19:27:42 -0300

    chore: add license notice per FSF recommendations

--- maestro.c
+++ maestro.c
@@ -1,3 +1,6 @@
+// Copyright (C) 2025  Gerson Ferreira <gersonfaneto.dev@gmail.com>
+// See end of file for extended copyright information.
+
 #define MAESTRO_IMPLEMENTATION
 #include "maestro.h"
 
@@ -7,5 +10,21 @@ int main(void)
 
     return 0;
 }
+
+// Maestro :: Self Hosted C Build System.
+// Copyright (C) 2025  Gerson Ferreira <gersonfaneto.dev@gmail.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with This program.  If not, see <https://www.gnu.org/licenses/>.
 
 // vim: sw=4 ts=4 sts=4 et tw=80 nospell :
--- maestro.h
+++ maestro.h
@@ -1,3 +1,6 @@
+// Copyright (C) 2025  Gerson Ferreira <gersonfaneto.dev@gmail.com>
+// See end of file for extended copyright information.
+
 #ifndef MAESTRO_H_
 #define MAESTRO_H_
 
@@ -13,5 +16,21 @@ static inline void say_hello(const char* name)
 #endif // ifdef MAESTRO_IMPLEMENTATION
 
 #endif // ifndef MAESTRO_H_
+
+// Maestro :: Self Hosted C Build System.
+// Copyright (C) 2025  Gerson Ferreira <gersonfaneto.dev@gmail.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with This program.  If not, see <https://www.gnu.org/licenses/>.
 
 // vim: sw=4 ts=4 sts=4 et tw=80 nospell :
