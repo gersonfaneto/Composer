b44aab9a09f9c24c25b2c5761222552528054508
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2025-05-23 10:00:28 -0300

    chore(*)!: swith to 'The Unlicense' for LICENSE

--- composer.c
+++ composer.c
@@ -1,6 +1,3 @@
-// Copyright (C) 2025  Gerson Ferreira <gersonfaneto.dev@gmail.com>
-// See end of file for extended copyright information.
-
 #define COMPOSER_IMPLEMENTATION
 #include "composer.h"
 
@@ -10,21 +7,5 @@ int main(void)
 
     return 0;
 }
-
-// Composer :: Orchestrate your build with C!
-// Copyright (C) 2025  Gerson Ferreira <gersonfaneto.dev@gmail.com>
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with This program.  If not, see <https://www.gnu.org/licenses/>.
 
 // vim: sw=4 ts=4 sts=4 et tw=80 nospell :
